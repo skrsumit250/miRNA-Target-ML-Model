6a7e52293a5d42eb04f01a3963138a13178d1e75
skrsumit250 <skrsumit250@gmail.com>
2025-04-23 03:24:36 +0530

    build pages

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes} from 'react-router-dom'
+import { BrowserRouter, HashRouter, Route, Routes} from 'react-router-dom'
 import Navbar from './assets/navbar';
 import Home from './assets/home';
 import Search from './assets/Search';
@@ -7,14 +7,14 @@ import './App.css'
 
 function App(){
     return(
-        <BrowserRouter>
+        <HashRouter>
             <Navbar/>
             <Routes>
                 <Route path='/' element={<Home/>}/>
                 <Route path='/search' element={<Search/>}/>
                 <Route path='/tool' element={<Tool/>}/>
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
     )
 }
 export default App;
